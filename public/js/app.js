fe031950e15ffcf12f7c93404ce10fd95b84aa4f
CORP\mlowen <anton@kivi.nu>
2024-04-11 02:10:23 +0200

    Function add

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,24 @@
 // Pages.Config //
 const Pages = {
     chat:{
-        acitve:true,
-        id:""
+        active:true,
+        id:"page_chat",
+        button:"page_chat_button"
     },
     main:{
-        acitve:false,
-        id:""
+        active:false,
+        id:"",
+        button:"page_main_button"
     },
     setting:{
-        acitve:false,
-        id:""
+        active:false,
+        id:"",
+        button:"page_setting_button"
     },
     account:{
-        acitve:false,
-        id:""
+        active:false,
+        id:"",
+        button:"page_account_button"
     }
 }
 
@@ -22,12 +26,62 @@ const Pages = {
 function notify(content) {
     var x = document.getElementById("snackbar");
     x.className = "show";
-    x.textContent = content
+    x.textContent = content;
+    const _notify = {
+        _header:"",
+        _content:content,
+        _time:getTimeString()
+    }
     setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+    return(_notify)
 }
-function switchPage(page)
+function switchPage(page) {
+    if(Pages.hasOwnProperty(page)) {
+        for (const pageKey in Pages) {
+            if (pageKey === page) {
+                Pages[pageKey].active = true;
+            } else {
+                Pages[pageKey].active = false;
+            }
+        }
+        console.log(`Switched to page: ${page}`);
+        console.log(Pages);
+        return true;
+    } else {
+        console.log(`Page '${page}' does not exist.`);
+        return false;
+    }
+}
+function toggle(id, s)
 {
-    return Pages.hasOwnProperty(pageName);
+    const element = document.getElementById(id);
+    if(element != null)
+    {
+        if(s != null)
+        {
+            if(s === false)
+            {
+                element.classList.remove("state_show");
+                element.classList.add("state_hide");
+                return false;
+            }else{
+                element.classList.add("state_show");
+                element.classList.remove("state_hide");
+                return true;
+            }
+        }else{
+            if(element.classList.contains("state_show"))
+            {
+                element.classList.remove("state_show");
+                element.classList.add("state_hide");
+                return false;
+            }else{
+                element.classList.add("state_show");
+                element.classList.remove("state_hide");
+                return true;
+            }
+        }
+    }
 }
 function chatForm(s)
 {
@@ -54,4 +108,13 @@ function chatForm(s)
             return false;
         }
     }
+}
+function getTimeString()
+{
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+
+    const _time = `${hours}:${minutes}`;
+    return _time;
 }
